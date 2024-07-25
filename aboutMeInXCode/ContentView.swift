a8ce0254d525f8cdef362935b9a95f8c440f76af
kbayyapu27 <kikirdy@gmail.com>
2024-07-25 09:35:11 -0500

    Initial Commit

---
+++ aboutMeInXCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aboutMeInXCode
+//
+//  Created by Krithika Bayyapu on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aboutMeInXCode/aboutMeInXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeInXCodeApp.swift
+//  aboutMeInXCode
+//
+//  Created by Krithika Bayyapu on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeInXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

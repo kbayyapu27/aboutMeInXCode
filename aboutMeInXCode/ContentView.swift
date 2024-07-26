964378f46d6c294d7e0d397f5cf09d22aace0490
kbayyapu27 <kikirdy@gmail.com>
2024-07-26 12:03:52 -0500

    trial

--- aboutMeInXCode/ContentView.swift
+++ aboutMeInXCode/ContentView.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+/*extension Color {
+    static let cream = Color(red: 209 , green: 209, blue: 194 )
+}*/
+
+struct ContentView: View
+{
+    
+    var body: some View
+    {
+        
+        ZStack {
+            Color(.systemGray5)
+                .ignoresSafeArea()
+                .cornerRadius(30)
+            VStack(alignment: .center, spacing: 20.0)
+            {
+                Text("Krithika Bayyapu")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(red: 0.113, green: 0.138, blue: 0.272))
+                    .padding(.vertical, 0.0)
+                
+                Image("krithika")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal)
+                    .cornerRadius(30)
+                
+                Text("A Little Bit About Me:")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.113, green: 0.138, blue: 0.272))
+                    .padding(.top, 1.0)
+                
+                Text("Hi, my name is Krithika! I am a rising sophmore at Stevenson High School in the suburbs of Chicago, IL. In my free time, I love meeting new people and experimenting with computer science! I also love to travel and bake desserts. I am super excited to connect with an amazing community through Kode With Klossy!")
+                    .font(.footnote)
+                    .foregroundColor(Color(red: 0.113, green: 0.138, blue: 0.272))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -9.0)
+                    .padding(.horizontal, 10.0)
+            }
         }
-        .padding()
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+                    .cornerRadius(30)
+                    .shadow(radius: 15)
+                    .preferredColorScheme(/*@START_MENU_TOKEN@*/.light/*@END_MENU_TOKEN@*/)
     }
 }
 
